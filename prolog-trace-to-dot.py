2ba279f19102397b988a7a75e7a9d319438435a1
no382001 <102482527+no382001@users.noreply.github.com>
2024-01-06 01:06:24 +0100

    wow

--- prolog-trace-to-dot.py
+++ prolog-trace-to-dot.py
@@ -1,14 +1,14 @@
 import pygraphviz as pgv
 
 graph = pgv.AGraph(strict=False, directed=True)
-
+graph.graph_attr['ranksep'] = '10.0'  # increases the space between ranks
+graph.node_attr['shape'] = 'ellipse'
 def get_first_word(line):
     if line == "":
         return
     words = line.split()
     if len(words) > 0:
-        first_word = words[0]
-        return first_word
+        return words[0]
 
 def add_node(graph, parent, line):
     node_name = line.strip()
@@ -17,22 +17,25 @@ def add_node(graph, parent, line):
         graph.add_edge(parent, node_name)
     return node_name
 
-
 with open("trace.txt", 'r') as trace_file:
-    current = None
+    call_stack = []
 
     while True:
         line = trace_file.readline()
         if line.strip() == "":
             break
         first_word = get_first_word(line)
-        if current is None or first_word == "Call:":
-            current = add_node(graph, current, line)
+        if first_word == "Call:":
+            current = add_node(graph, call_stack[-1] if call_stack else None, line)
+            call_stack.append(current)
         elif first_word in ["Exit:", "Fail:"]:
-            add_node(graph, current, line)
-            current = None  # or set to the parent node if you have a way to track it
+            if call_stack:
+                add_node(graph, call_stack[-1], line)
+                call_stack.pop()
         elif first_word == "Redo:":
-            current = add_node(graph, current, line)
+            current = add_node(graph, call_stack[-1] if call_stack else None, line)
 
+# try for best result
+# sccmap, dot, acyclic, ccomps, osage, gc, neato, gvpr, nop, patchwork, twopi, sfdp, tred, gvcolor, circo, fdp, unflatten 
 graph.layout(prog='dot')
 graph.write('call_tree.dot')
